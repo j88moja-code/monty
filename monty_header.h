688be116f317355e6c11dfbf264ceb9e5e2396e5
j88moja-code <sitholent+github@gmail.com>
2022-09-08 09:58:07 -0700

    Update for advanced tasks opcodes

--- monty_header.h
+++ monty_header.h
@@ -101,6 +101,9 @@ void _pop(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
 void m_nop(stack_t **stack, unsigned int line_number);
-
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 
 #endif /* _MONTY_HEADER_ */
