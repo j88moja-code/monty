1126399fe5812504eefca68e01ed8adcd7d58abd
j88moja-code <sitholent+github@gmail.com>
2022-09-08 08:40:10 -0700

    Add file for functions that free stack and queues

---
+++ init_functions.c
@@ -0,0 +1,100 @@
+#include "monty_header.h"
+
+/**
+ * free_lineptr - free line pointer returned by getline
+ * @status: exit status
+ * @arg: pointer to line
+ *
+ * Return: void
+ */
+void free_lineptr(int status, void *arg)
+{
+	char **lineptr = arg;
+
+	(void)status;
+	if (*lineptr != NULL)
+		free(*lineptr);
+}
+
+/**
+ * free_stack - frees the stack on exit
+ * @status: exit status
+ * @arg: double pointer to the stack
+ *
+ * Return: void
+ */
+void free_stack(int status, void *arg)
+{
+	stack_t **stack;
+	stack_t *next;
+
+	(void)status;
+
+	stack = (stack_t **)arg;
+	if (*stack)
+	{
+		(*stack)->prev->next = NULL;
+		(*stack)->prev = NULL;
+	}
+	while (*stack != NULL)
+	{
+		next = (*stack)->next;
+		free(*stack);
+		*stack = next;
+	}
+	var.stack_len = 0;
+}
+
+/**
+ * add_node - add a new node to a circular linked list
+ * @stack: double pointer to the beginning of the circular linked list
+ * @n: value to add to the new node
+ *
+ * Description: the function will add the node to the begining if in
+ * stack mode and the end if in queue mode
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+stack_t *add_node(stack_t **stack, const int n)
+{
+	stack_t *new;
+
+	if (stack == NULL)
+		return (NULL);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (*stack == NULL)
+	{
+		new->prev = new;
+		new->next = new;
+	}
+	else
+	{
+		(*stack)->prev->next = new;
+		new->prev = (*stack)->prev;
+		(*stack)->prev = new;
+		new->next = *stack;
+	}
+	if (var.queue == STACK || var.stack_len == 0)
+		*stack = new;
+	return (new);
+}
+
+/**
+ * m_fs_close - close file stream
+ * @status: status passed to exit
+ * @arg: pointer to file stream
+ *
+ * Return: void
+ */
+void m_fs_close(int status, void *arg)
+{
+	FILE *fs;
+
+	(void)status;
+
+	fs = (FILE *) arg;
+	fclose(fs);
+}
