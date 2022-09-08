a84876922ec3cbba5ca9fa7b4fad2c926ce02160
j88moja-code <sitholent+github@gmail.com>
2022-09-08 10:22:09 -0700

    Update for new opcodes

--- monty_3functions.c
+++ monty_3functions.c
@@ -26,3 +26,46 @@ void _mod(stack_t **stack, unsigned int line_number)
 	(*stack)->next->next->n %= (*stack)->next->n;
 	_pop(stack, line_number);
 }
+
+/**
+ * _pchar - Prints the character in the top value
+ *               node of a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL)
+	{
+		set_op_tok_error(pchar_error(line_number, "stack empty"));
+		return;
+	}
+	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
+	{
+		set_op_tok_error(pchar_error(line_number,
+					     "value out of range"));
+		return;
+	}
+
+	printf("%c\n", (*stack)->next->n);
+}
+
+/**
+ * _pstr - Prints the string contained in a stack_t linked list.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp && tmp->n != 0 && (tmp->n > 0 && tmp->n <= 127))
+	{
+		printf("%c", tmp->n);
+		tmp = tmp->next;
+	}
+
+	printf("\n");
+
+	(void)line_number;
+}
--- monty_header.h
+++ monty_header.h
@@ -105,5 +105,7 @@ void _sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
 
 #endif /* _MONTY_HEADER_ */
