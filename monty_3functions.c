85c19ba7c1e82a9e497dbd193400451d66f831d5
j88moja-code <sitholent+github@gmail.com>
2022-09-09 23:09:12 -0700

    Add new functions

--- monty_3functions.c
+++ monty_3functions.c
@@ -1,7 +1,7 @@
 #include "monty_header.h"
 
 /**
- * monty_mod - Computes the modulus of the second value from the
+ * _mod - Computes the modulus of the second value from the
  *             top of a stack_t linked list  by the top value.
  * @stack: A pointer to the top mode node of a stack_t linked list.
  * @line_number: The current working line number of a Monty bytecodes file.
@@ -66,6 +66,61 @@ void _pstr(stack_t **stack, unsigned int line_number)
 	}
 
 	printf("\n");
+
+	(void)line_number;
+}
+
+/**
+  * _rotl - rotates the top value of stack_t linked list to the bottom.
+  *
+  * @stack: pointer to the top mode node of a stack_t linked list.
+  * @line_number: current working line number of a Monty bytecode file.
+  *
+  * Return: nothing (void).
+  */
+void _rotl(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	top->next->prev = *stack;
+	(*stack)->next = top->next;
+	bottom->next = top;
+	top->next = NULL;
+	top->prev = bottom;
+
+	(void)line_number;
+}
+
+/**
+ * _rotr - Rotates the bottom value of a stack_t linked list to the top.
+ * @stack: A pointer to the top mode node of a stack_t linked list.
+ * @line_number: The current working line number of a Monty bytecodes file.
+ */
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *top, *bottom;
+
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+		return;
+
+	top = (*stack)->next;
+	bottom = (*stack)->next;
+	while (bottom->next != NULL)
+		bottom = bottom->next;
+
+	bottom->prev->next = NULL;
+	(*stack)->next = bottom;
+	bottom->prev = *stack;
+	bottom->next = top;
+	top->prev = bottom;
 
 	(void)line_number;
 }
